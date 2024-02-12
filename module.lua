54d5421745fb1f2ac8d6eecce9b1a2c660189ab0
fucksource2 <157608686+fucksource2@users.noreply.github.com>
2024-02-12 15:35:49 -0800

    Update module.lua

--- module.lua
+++ module.lua
@@ -14,7 +14,7 @@ local signature_jmpecx = client_find_signature("engine.dll", "\xFF\xE1") or erro
 local signature_GetModuleHandle = client_find_signature("engine.dll", "\xFF\x15\xCC\xCC\xCC\xCC\x85\xC0\x74\x0B") or error "engine.dll!::GetModuleHandle couldn't be found"
 local signature_GetProcAddress = client_find_signature("engine.dll", "\xFF\x15\xCC\xCC\xCC\xCC\xA3\xCC\xCC\xCC\xCC\xEB\x05") or error "engine.dll!::GetProcAddress couldn't be found"
 
--- Thunk function for calling a function pointer with specified address and typestring.
+-- Define a thunk function for calling function pointers with specified address and typestring
 local thunk = function(address, typestring)
     return function(...) return ffi_cast(typestring, signature_jmpecx)(address, ...) end
 end
@@ -23,10 +23,11 @@ end
 local native_GetModuleHandle = thunk(ffi_cast("void***", ffi_cast("uint32_t", signature_GetModuleHandle) + 2)[0][0], "void*(__thiscall*)(void*, const char*)")
 local native_GetProcAddress = thunk(ffi_cast("void***", ffi_cast("uint32_t", signature_GetProcessAddress) + 2)[0][0], "void*(__thiscall*)(void*, void*, const char*)")
 
--- Bind function to retrieve function pointers by module name, address, and typestring.
+-- Define a bind function to retrieve function pointers by module name, address, and typestring
 local bind = function(module_name, address, typestring)
     local handle = native_GetModuleHandle(module_name)
     return function(...) return thunk(native_GetProcessAddress(handle, address), typestring) end
 end
 
-local native_FindWindowA = bind("kernel32.dll", "FindWindowA", "void*(__thiscall*)(void*, const char*, const char*)") -- example
+-- Example usage: binding FindWindowA function from kernel32.dll
+local native_FindWindowA = bind("kernel32.dll", "FindWindowA", "void*(__thiscall*)(void*, const char*, const char*)")
